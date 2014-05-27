04057351e15a97621a14a22d044f6277f3fde2ab
patthebug <Dashingparrot1>
2014-05-27 15:41:54 -0700

    my pelican
    my pelican

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,3 +29,6 @@ DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Theme
+THEME = "/home/Prateek/pelican-themes/mnmlist"
