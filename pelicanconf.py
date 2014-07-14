49682afc60fab70289714b388d060723a42a8c84
patthebug <Dashingparrot1>
2014-07-13 17:23:48 -0700

    theme
    theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,7 +30,7 @@ DEFAULT_PAGINATION = 5
 #RELATIVE_URLS = True
 
 # Theme
-THEME = "/home/Prateek/pelican-themes/nmnlist"
+THEME = "/home/Prateek/pelican-themes/pelican-bootstrap3"
 STATIC_PATHS = ['extra']
 EXTRA_PATH_METADATA = {
 'extra/CNAME': {'path':'CNAME'}}
